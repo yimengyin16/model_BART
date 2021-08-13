6a6e2b996259e4ba4b49c5897a669de7dcea3197
Yimeng Yin <marshallpku@gmail.com>
2021-08-13 11:18:59 -0400

    add initial files
    use PERF A model as tempalte

---
+++ model/simulation/model_sim_master.R
@@ -0,0 +1,169 @@
+# Actuarial valuation for PERF A with 2-tier simplification
+
+
+# Valuation name
+# rm(list = ls())
+
+#source("libraries.R")
+
+#*******************************************************************************
+#                           ### Valuation parameters ####                      
+#*******************************************************************************
+## File path of the run control file
+
+# dir_runControl <- "model/"
+# fn_runControl  <- "RunControl.xlsx"
+# filePath_runControl <- paste0(dir_runControl, fn_runControl)
+# 
+# dir_outputs <- "model/simulation/outputs_sim/"
+# 
+# 
+# ## Import global parameters
+# Global_paramlist <- read_excel(filePath_runControl, sheet="GlobalParams") %>% 
+#   filter(!is.na(init_year)) %>% 
+#   as.list
+#  
+# ## Import valuation parameters
+# sim_paramlist <- read_excel(filePath_runControl, sheet="params_sim", skip  = 3) %>% 
+#   filter(!is.na(sim_name), include == TRUE) %>% 
+#   as.list
+# 
+# ## Import investment return scenarios
+# returnScenarios <- read_excel(filePath_runControl, sheet="returns", skip = 0) %>% filter(!is.na(scenario))
+# 
+# 
+# sim_name_run <- sim_paramlist$sim_name #"Dev_cola"
+
+
+## Additinal global variables 
+
+# # age and entry age ranges
+# Global_paramlist$range_age <- with(Global_paramlist, min_age:max_age)
+# Global_paramlist$range_ea  <- with(Global_paramlist, min_ea:max_ea)
+
+
+# turn tier names into a character vector
+
+
+
+cat("Running simulation", sim_paramlist$sim_name, "\n")
+
+#*******************************************************************************
+#                         Load data                                         ####
+#*******************************************************************************
+
+# sim_paramlist$run_val <- FALSE
+# if(sim_paramlist$run_val){
+#   source(paste0("model/valuation/model_val_create_", sim_paramlist$val_name, ".R"))
+# }
+
+# Load tier data
+dir_val <- "model/valuation2/outputs_val/"
+
+# tierData_miscAll
+
+#*******************************************************************************
+#              Actual investment return, for all tiers                      ####
+#*******************************************************************************
+source("model/simulation/model_sim_invReturns.R")
+sim_paramlist$seed <- 123
+i.r <- gen_returns()
+#i.r
+i.r[1:10, 1:5]
+
+
+#*******************************************************************************
+#                          Simulation ####
+#*******************************************************************************
+
+# if(sim_paramlist$useContingentCOLA){
+#   source("model/simulation/model_sim_simulation_contingentCOLA.R")
+# } else {
+#   source("model/simulation/model_sim_simulation.R")
+# }
+source("model/simulation/model_sim_simulation_contingentCOLA(7).R")
+
+
+{
+  start_time <- Sys.time()	
+  penSim_results <- run_sim()
+  end_time <- Sys.time()
+  print(end_time  - start_time)
+  suppressMessages(gc())
+}
+
+
+
+
+
+
+#*******************************************************************************
+#                        Saving results ####
+#*******************************************************************************
+
+outputs_list <- list(sim_paramlist    = sim_paramlist,
+                     Global_paramlist = Global_paramlist,
+                     results          = penSim_results)
+
+
+saveRDS(outputs_list, file = paste0(dir_outputs, "sim_", sim_name_run, ".rds"))
+
+
+
+
+
+#*******************************************************************************
+#                        TEMP: Examine results  ####
+#*******************************************************************************
+
+
+# Display1 Basic examination
+var_display1 <- c("sim_name", "val_name", "sim", "year", 
+                  "AL", "FR_MA",  "UAAL", "ERC", "ERC_PR", "NC",
+                  "MA",
+                  "AL", 
+                  "AL.active", "AL.nonactive",
+                  "AL.defrRet",
+                  "AL.servRet",
+                  "PVFB",
+                  "PVFB.active",
+                  "cola_actual",
+                  "B",
+                  "NC_PR",
+                  "ERC_PR",
+                  "EEC_PR",
+                  # "ADC", 
+                  "NC", "ERC", "EEC", "SC", "LG", "i.r", "PR",
+                  "nactives"
+)
+
+
+# Display: Decomposition of AL and PVFB
+#var_display_decomp_Liab <- c("sim", "year", "FR_MA", "PVFB.act.laca", "PVFB.act.v", "PVFB.act.disbRet", "PVFB.act.death")
+
+# Display: Decompsition of Benefit
+#var_display_decomp_B <- c( "sim", "year", "FR_MA", "AL.act.death", "NC.death", "AL.death", "B.death")
+
+# Display: demograhpics
+#var_display_demo <- c("sim", "year", "FR_MA", "nactives", "nla", "nterms", "ndisbRet")
+# "n.ca.R1", "n.ca.R0S1", "nterms",
+# "ndisb.la", "ndisb.ca.R1", "ndisb.ca.R0S1" )
+
+penSim_results %>% filter(sim == 0)  %>% select(one_of(var_display1))  %>% print
+# penSim_results %>% filter(sim == 1)  %>% select(one_of(var_display1))  %>% print
+# penSim_results %>% filter(sim == -2) %>% select(one_of(var_display1))  %>% print
+
+print(end_time  - start_time)
+
+
+
+# 
+# penSim_results %>% filter(sim == 0, year == max(year)) %>% 
+#   mutate(defr_pct.active = AL.defrRet/AL.active,
+#          defr_pct_servRet = AL.defrRet / AL.servRet)
+
+
+# sim_misc_bf100_cola2$results   %>% filter(sim == 0, year <= 2027)  %>% select(one_of(var_display1))  %>% print
+# sim_misc_bf100_colaCut$results %>% filter(sim == 0, year <= 2027)  %>% select(one_of(var_display1))  %>% print
+
+
